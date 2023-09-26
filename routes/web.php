62c395920701dfc559e44d02fdbf355f169dd610
ftvalex01 <ftvalex@hotmail.com>
2023-09-26 17:44:11 +0100

    Ejercicio 2 terminado

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user/{name?}', function (?string $name = null) {
+    return $name;
+});
+Route::get('/user/{name?}', function (?string $name = 'pepe') {
+    return $name;
+});
+
+Route::post('/reverse-me', function (Request $request) {
+    $reversed = strrev($request->input('reverse_this'));
+    return $reversed;
+});
+
+  Route::match(array('GET', 'POST'), '/ambas', function()
+{
+    return 'Hello World';
+});
+
+Route::get('/usuario/{id}', function (string $id) {
+    return "hola usuario $id"; 
+})->where('id', '[0-9]+');
+
+Route::get('/user/{name}/{id}', function (string $name, string $id) {
+    return "hola usuario $name con id:$id";
+})->where(['name' => '[a-z]+','id' => '[0-9]+']);
+
+// ejercicio 2
+
+Route::get('/host', function () {
+    return env('DB_HOST');
+});
+Route::get('/timezone', function () {
+    return config('app.timezone');
+});
+
